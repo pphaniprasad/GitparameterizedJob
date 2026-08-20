0356fa4f703e8db6cec41fa3792b4b61bdf1ab66
pphaniprasad <phaniprasad_cse@mvsrec.edu.in>
2026-08-20 23:36:40 +0530

    first

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 import sys
 print("Addition sing command line arguments")
-a=int(sys(argv[1]))
-b=int(sys(argv[2]))
+a=int(sys.argv[1])
+b=int(sys.argv[2])
 print("Addr result=",(a+b))
 print("Completed")
